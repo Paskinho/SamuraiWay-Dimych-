07760986e64e752bcb3600490516c063e21ec6ac
Пасканов Владислав Витальевич <paskanov@Unibelus.local>
2022-10-13 11:14:21 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+      <div>
+          <Header />
+          <Technologies />
+      </div>
+  );
+}
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li> css</li>
+                <li> html</li>
+                <li> jss</li>
+            </ul>
+        </div>
+    )
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">News Feed</a>
+            <a href="#s">Message</a>
+        </div>
+    )
+}
+
+export default App;
